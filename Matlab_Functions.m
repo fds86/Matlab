e967d7a9a7fda4a627f91cfbd2b2fa38f148002b
fds86 <francescodalsanto@gmail.com>
2025-01-19 20:57:59 +0000

    Update functions list

--- Matlab_Functions.m
+++ Matlab_Functions.m
@@ -13,6 +13,19 @@ clear var1 var2 %elimina le variabili var1 e var2.
 
 %% Istruzioni per Sistemi di Controllo.
 
+impulse (num, den, t) % calcola la risposta all’impulso del sistema la cui 
+                      % funzione di trasferimento è descritta dai polinomi 
+                      % num e den. t è il vettore che definisce il tempo.
+
+step (num, den, t) % calcola la risposta allo scalino del sistema la cui 
+                   % funzione di trasferimento è descritta dai polinomi 
+                   % num e den. t è il vettore che definisce il tempo.
+
+lsim (num, den, u, t) % calcola la risposta del sistema la cui funzione di 
+                      % trasferimento è descritta dai polinomi num e den 
+                      % all’ingresso u. t è il vettore che definisce il
+                      % tempo.
+
 bode (num, den, w) % calcola modulo e fase della funzione di trasferimento 
                    % descritta dai polinomi num e den in corrispondenza 
                    % delle pulsazioni specificate dal vettore w (omega).
@@ -34,3 +47,14 @@ rlocus (num, den) % traccia il luogo delle radici della funzione di
                   % trasferimento descritta dai polinomi num e den 
                   % utilizzando un vettore dei guadagni determinato
                   % automaticamente.
+
+tf(num,den) % crea una funzione di trasferimento a tempo continuo 
+            % con numeratore e denominatore specificato dai parametri 
+            % num e den.
+
+ss(A,B,C,D) % crea un oggetto che rappresenta un modello nello spazio 
+            % di stato di un sistema a tempo continuo. In input vengono 
+            % passate le 4 matrici A,B,C,D.
+
+zpk(z,p,k) % crea un oggetto che rappresenta un sistema a tempo continuo 
+           % passando in input gli zeri z, i poli p e il guadagno k.
